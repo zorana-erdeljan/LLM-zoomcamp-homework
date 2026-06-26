12aa43e9c44a6b80f0302047c417606fda5d00ad
zorana-erdeljan <97516486+zorana-erdeljan@users.noreply.github.com>
2026-06-26 15:30:30 +0000

    01 homework

---
+++ 01/rag_helper.py
@@ -0,0 +1,81 @@
+INSTRUCTIONS = '''
+Your task is to answer questions from the course participants
+based on the provided context.
+
+Use the context to find relevant information and provide accurate
+answers. If the answer is not found in the context,
+respond with "I don't know."
+Also include the filename where the answer was found.
+'''
+
+PROMPT_TEMPLATE = '''
+QUESTION: {question}
+
+CONTEXT:
+{context}
+'''.strip()
+
+
+class RAGBase:
+
+    def __init__(
+        self,
+        index,
+        llm_client,
+        instructions=INSTRUCTIONS,
+        prompt_template=PROMPT_TEMPLATE,
+        model='gpt-5.4-mini'
+    ):
+        self.index = index
+        self.llm_client = llm_client
+        self.instructions = instructions
+        self.prompt_template = prompt_template
+        self.model = model
+
+   
+    
+    def search(self, query, num_results=5):
+        return self.index.search(
+            query,
+            num_results=num_results
+    )
+
+    def build_context(self, search_results):
+        lines = []
+
+        for doc in search_results:
+            lines.append('Filename: ' + doc['filename'])
+            lines.append('Content: ' + doc['content'])
+            lines.append('')
+
+        return '\n'.join(lines).strip()
+
+    def build_prompt(self, query, search_results):
+        context = self.build_context(search_results)
+        return self.prompt_template.format(
+            question=query, context=context
+        )
+
+    def llm(self, prompt):
+        input_messages = [
+            {'role': 'developer', 'content': self.instructions},
+            {'role': 'user', 'content': prompt}
+        ]
+
+        response = self.llm_client.responses.create(
+            model=self.model,
+            input=input_messages
+        )
+
+        usage = response.usage
+
+       
+
+
+        return response.output_text, usage
+
+    def rag(self, query):
+        search_results = self.search(query)
+        prompt = self.build_prompt(query, search_results)
+        answer, usage = self.llm(prompt)
+        return answer, usage
